07076f416e5ede1cd42ccaa118827e6862049046
Amancio302 <matheusamancio.dev@gmail.com>
2020-08-25 20:26:05 -0300

    Sidebar not showing icons bug resolved

--- src/views/Gerenciar_Produtos.php
+++ src/views/Gerenciar_Produtos.php
@@ -202,6 +202,7 @@
                     <script src=\"https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js\"></script>
                     <script src=\"https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js\"></script>
                     <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js\"></script>
+                    <script src=\"../views/js/dashboard.js\"></script>
                 
                 </body>
             </html>     
