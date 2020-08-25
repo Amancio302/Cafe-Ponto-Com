12e1993e0e5bcda4b825c364e78bcabc17ad0e5b
Amancio302 <matheusamancio.dev@gmail.com>
2020-08-25 12:32:52 -0300

    New generic classes implemented

--- src/controllers/UsuarioController.php
+++ src/controllers/UsuarioController.php
@@ -4,17 +4,17 @@
     require_once("../persistance/UsuarioDAO.php");
 
     class UsuarioController extends Controller {
-        
-        private $persistance;
 
         function __construct () {
-            echo 3;
             $this->persistance = new UsuarioDAO();
-            echo 4;
         }
 
         function login ($email, $senha) {
             return $this->persistance->login($email, $senha);
+        }
+
+        function getNome ($idUsuario) {
+            return $this->persistance->getOneUsuario($idUsuario)->Nome;
         }
         
     }
