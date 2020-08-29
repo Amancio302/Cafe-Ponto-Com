4a4c422728728006b09ff388702034697523f717
Matheus <matheus.ferreira3@estudante.ufla.br>
2020-08-29 10:21:12 -0300

    PedidoDAO may  calls VendaDAO update

--- src/persistance/PedidoDAO.php
+++ src/persistance/PedidoDAO.php
@@ -7,16 +7,25 @@ require_once("../models/Pedido.php");
 class PedidoDAO extends Database_Connect{
 
     public function createPedido($idVenda, $idProduto, $qtdProduto) {
+        // Conecta ao BD
         $connection = $this->connect();
+        // Monta o Script Sql
         $data = "($idVenda, $idProduto, $qtdProduto)";
         $sql = "INSERT INTO Pedido (idVenda, idProduto, qtdProduto) VALUES $data";
+        // Executa a Query
         $connection->query($sql);
+        // Resgata qual o id inserido
         $idPedido = mysqli_insert_id($connection);
+        // Fecha a conexão
         mysqli_close($connection);
+        // Atualiza o valor total da venda relacionada
         $Venda = new VendaDAO();
         $venda = $Venda->getOneVenda($idVenda);
         $Produto = new ProdutoDAO();
         $produto = $Produto->getOneProduto($idProduto);
+        $venda->Valor_Total = $venda->Valor_Total + ($produto->Preco * qtdProduto);
+        $Venda->updateVenda($idVenda, $venda);
+        // Contrói um objeto Pedido para ser retornado
         return new Pedido($idPedido, $venda, $produto, $qtdProduto);
     }
 
@@ -45,21 +54,40 @@ class PedidoDAO extends Database_Connect{
     }
 
     public function updatePedido($idPedido, $PedidoData) {
+        // Conecta ao BD
         $connection = $this->connect();
+        // Resgata dados pertinentes de PedidoData
         $idVenda = $PedidoData->Venda->idVenda;
         $idProduto = $PedidoData->Produto->idProduto;
+        $precoantigo = $PedidoData->Produto->Preco * $Pedido->qtdProduto;
+        // Monta o Script Sql
         $data = "idVenda = $idVenda, idProduto = $idProduto, qtdProduto = $PedidoData->qtdProduto";
         $sql = "UPDATE Pedido SET $data WHERE idPedido = $idPedido";
-        $res = mysqli_query($connection, $sql);
+        // Executa a Query
+        $res = $connection->query($sql);
+        // Fecha a conexão
         mysqli_close($connection);
+        // Atualiza o valor total da venda relacionada
+        $Venda = new VendaDAO();
+        $venda = $Venda->getOneVenda($idVenda);
+        $venda->Valor_Total = $Venda->Valor_Total - $precoantigo + ($PedidoData->Produto->Preco * $PedidoData->qtdProduto);
+        $Venda->updateVenda($idVenda, $venda);
         return $this->getOnePedido($idPedido);
     }
 
     public function deletePedido($idPedido) {
+        // Resgata os dados do pedido deletado para ser retornado
         $deleted = $this->getOnePedido($idPedido);
+        // Conecta ao BD
         $connection = $this->connect();
+        // Monta o Script SQL
         $sql = "DELETE FROM Pedido WHERE idPedido = $idPedido";
+        // Executa a Query
         mysqli_query($connection, $sql);
+        // Fecha a conexão
+        $Venda = new VendaDAO();
+        $venda = $Venda->getOneVenda($deleted->Venda->idVenda);
+        $venda->Valor_Total = $venda->Valor_Total - ($deleted->Produto->Preco * $deleted->qtdProduto);
         mysqli_close($connection);
         return $deleted;
     }
